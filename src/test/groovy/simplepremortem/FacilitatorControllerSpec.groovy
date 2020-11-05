28a12721280f67510be1bd6546ea314681b8e009
Robert Pastel <pastel@mtu.edu>
2020-11-05 16:35:38 -0500

    Just the Spec

---
+++ src/test/groovy/simplepremortem/FacilitatorControllerSpec.groovy
@@ -0,0 +1,18 @@
+package simplepremortem
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class FacilitatorControllerSpec extends Specification implements ControllerUnitTest<FacilitatorController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
