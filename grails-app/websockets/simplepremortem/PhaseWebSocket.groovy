813313ed0e1f3282bb2d8579b52d1603a901693f
Robert Pastel <pastel@mtu.edu>
2020-11-06 09:57:19 -0500

    Added server context path, cleaned code, commentting

--- grails-app/websockets/simplepremortem/PhaseWebSocket.groovy
+++ grails-app/websockets/simplepremortem/PhaseWebSocket.groovy
@@ -1,15 +1,4 @@
 package simplepremortem
 
-import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.SendTo
-
 class PhaseWebSocket {
-
-    @MessageMapping("/phase")
-    @SendTo("/topic/phase")
-    String phase(String message) {
-        println 'In PhaseWebSocket, message: '+ message
-        return message
-    }
-
 }
